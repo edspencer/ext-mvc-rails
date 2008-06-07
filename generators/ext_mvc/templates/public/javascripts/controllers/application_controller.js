3cc84596f678fd0d7aaf953a02835f19614642c0
Edward Spencer <edward@midlandswebdesign.com>
2008-06-07 14:48:10 +0100

    Removed defunct EXT MVC files

--- generators/ext_mvc/templates/public/javascripts/controllers/application_controller.js
+++
@@ -1,8 +0,0 @@
-ApplicationController = function(config) {
-  
-  this.application = application;
-
-  this.showPanel = function(panel) {
-    this.application.getLayoutManager().showPanel(panel);
-  };
-};
